5cba9821fb76429d3112dd81bd63a1f0d374bfd5
L3Mars <pain.antoine@gmail.com>
2024-08-31 00:07:46 +0200

    Review style

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,6 @@
 <template>
-  <pre>{{ question.data.libelle }}</pre>
-  <v-item-group mandatory v-model="selectedResponse" class="ma-5">
-    <v-item
-      v-for="proposition in question.data.proposition"
-      v-slot="{ isSelected, toggle }"
-    >
-      <v-btn
-        style="width: 200px; margin-bottom: 5px; display: block"
-        :value="proposition.id"
-        :variant="isSelected ? 'tonal' : 'outlined'"
-        :color="isSelected ? 'green' : 'blue'"
-        @click="toggle(proposition.id)"
-      >
-        {{ proposition.value }}
-      </v-btn>
-    </v-item>
-  </v-item-group>
-  <v-btn
-    @click="validateResponse()"
-    class="ma-5"
-    style="width: 200px"
-    color="green"
-  >
-    Valider
-  </v-btn>
-  {{ result }}
+  <v-card flat rounded class="mx-auto my-auto pa-5">
+    <Question></Question>
+  </v-card>
 </template>
-<script setup>
-const { data: question } = await useFetch("/api/question/question");
-const result = ref("");
-const selectedResponse = ref();
-
-function validateResponse() {
-  if (
-    question.value.data.proposition[selectedResponse.value].id ==
-    question.value.data.response
-  )
-    result.value = "Bravo !";
-  else result.value = "Looser !";
-}
-</script>
+<script setup></script>
