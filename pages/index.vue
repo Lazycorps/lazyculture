a77412df8c63e4e36cac074af381a88d4a3fc32f
L3Mars <pain.antoine@gmail.com>
2024-08-30 07:19:07 +0200

    Change button groupe

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,26 @@
 <template>
   <pre>{{ question.data.libelle }}</pre>
-  <v-btn-toggle v-model="selectedResponse" divided class="d-flex">
-    <v-btn
+  <v-item-group mandatory v-model="selectedResponse" class="ma-5">
+    <v-item
       v-for="proposition in question.data.proposition"
-      style="width: 200px; margin-bottom: 5px; display: block"
-      :value="proposition.id"
-      variant="outlined"
+      v-slot="{ isSelected, toggle }"
     >
-      {{ proposition.value }}
-    </v-btn>
-  </v-btn-toggle>
+      <v-btn
+        style="width: 200px; margin-bottom: 5px; display: block"
+        :value="proposition.id"
+        :variant="isSelected ? 'tonal' : 'outlined'"
+        :color="isSelected ? 'green' : 'blue'"
+        @click="toggle(proposition.id)"
+      >
+        {{ proposition.value }}
+      </v-btn>
+    </v-item>
+  </v-item-group>
   <v-btn
     @click="validateResponse()"
-    style="width: 200px; margin-bottom: 5px"
-    variant="outlined"
+    class="ma-5"
+    style="width: 200px"
+    color="green"
   >
     Valider
   </v-btn>
@@ -25,7 +32,10 @@ const result = ref("");
 const selectedResponse = ref();
 
 function validateResponse() {
-  if (selectedResponse.value == question.value.data.response)
+  if (
+    question.value.data.proposition[selectedResponse.value].id ==
+    question.value.data.response
+  )
     result.value = "Bravo !";
   else result.value = "Looser !";
 }
