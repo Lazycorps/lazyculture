d261f7f786dfbc2a1ff5f6f2e62330b712cf00d1
L3Mars <pain.antoine@gmail.com>
2024-08-30 06:25:39 +0200

    config vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@prisma/nuxt", "vuetify-nuxt-module"],
+  css: ["vuetify/styles", "@/assets/styles/main.scss"],
+  modules: [
+    "@prisma/nuxt",
+
+    async (options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) =>
+        // @ts-ignore
+        config.plugins.push(
+          vuetify({
+            autoImport: true,
+            styles: {
+              configFile: "assets/styles/vuetify.scss",
+            },
+          })
+        )
+      );
+    },
+  ],
+  vite: {
+    ssr: {
+      noExternal: ["vuetify"],
+    },
+  },
 });
