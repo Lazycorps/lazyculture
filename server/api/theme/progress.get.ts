0459b54cf85d6fa74d8cf871a3fae7a778628634
Antoine Pain <apain@biac.lu>
2024-09-25 09:47:58 +0200

    Review mastery

--- server/api/theme/progress.get.ts
+++ server/api/theme/progress.get.ts
@@ -1,3 +1,4 @@
+import { Theme } from "./../../../models/theme";
 import { PrismaClient } from "@prisma/client";
 import { serverSupabaseClient } from "#supabase/server";
 
@@ -7,7 +8,7 @@ export default defineEventHandler(async (event) => {
   const client = await serverSupabaseClient(event);
   const userConnected = (await client.auth.getUser())?.data?.user;
   const query = getQuery(event);
-  
+
   const questionCount = await prisma.question.count({
     where: {
       data: {
@@ -16,34 +17,68 @@ export default defineEventHandler(async (event) => {
       },
     },
   });
-  
+
   // Étape 1 : Récupérer toutes les réponses avec leurs questions associées
   let responseCount = 0;
   let mastery = 0;
-  if(userConnected != null){
-      const responses = await prisma.questionResponse.findMany({
-        include: {
-          question: true, // Inclure les questions pour accéder au champ JSON 'data'
-        },
-        where: {
-          question: {
-            data: {
-              path: ["theme"],
-              array_contains: query.theme as string,
-            },
-          },
-          userId: userConnected?.id,
-          // success: true,
-        },
-        orderBy: [{date:"desc"}],
-        distinct: ["questionId"],
-      });
-    
-    responseCount = responses.filter((r) => r.success).length;
-    const lastResponse = responses.slice(0, 50);
-    const goodResponse = lastResponse.filter(r => r.success).length;
-    mastery = lastResponse.length > 20 ? goodResponse / lastResponse.length * 10 : 0;
+  if (userConnected != null) {
+    const responses = await getAllSuccessResponses(
+      userConnected?.id,
+      query.theme as string
+    );
+    const lastResponse = await getLastResponses(
+      userConnected?.id,
+      query.theme as string,
+      50
+    );
+    responseCount = responses.length;
+    const goodResponseCount = lastResponse.filter((r) => r.success).length;
+    mastery =
+      lastResponse.length > 20
+        ? (goodResponseCount / lastResponse.length) * 10
+        : 0;
   }
 
   return { questionCount, responseCount, mastery };
 });
+
+async function getAllSuccessResponses(userConnected: string, theme: string) {
+  return await prisma.questionResponse.findMany({
+    include: {
+      question: true, // Inclure les questions pour accéder au champ JSON 'data'
+    },
+    where: {
+      question: {
+        data: {
+          path: ["theme"],
+          array_contains: theme as string,
+        },
+      },
+      userId: userConnected,
+      success: true,
+    },
+    distinct: ["questionId"],
+  });
+}
+async function getLastResponses(
+  userConnected: string,
+  theme: string,
+  take: number
+) {
+  return await prisma.questionResponse.findMany({
+    include: {
+      question: true, // Inclure les questions pour accéder au champ JSON 'data'
+    },
+    where: {
+      question: {
+        data: {
+          path: ["theme"],
+          array_contains: theme as string,
+        },
+      },
+      userId: userConnected,
+    },
+    orderBy: [{ date: "desc" }],
+    take,
+  });
+}
