c2fddfade8e9c06f2ed11fa494ade149af768604
L3Mars <pain.antoine@gmail.com>
2024-10-09 22:47:12 +0200

    Add daily series first step

---
+++ server/api/series/daily.get.ts
@@ -0,0 +1,87 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { serverSupabaseClient } from "#supabase/server";
+import prisma from "~/lib/prisma";
+
+export default defineEventHandler(async (event) => {
+  //   const client = await serverSupabaseClient(event);
+
+  const today = new Date().toJSON().slice(0, 10);
+  const currentDailySeries = await prisma.questionSeries.findFirst({
+    where: { date: new Date(today) },
+  });
+  if (currentDailySeries) return currentDailySeries;
+  else {
+    let dailyNumber = await prisma.questionSeries.count({
+      where: { type: "daily" },
+    });
+    dailyNumber++;
+    const questionsIds = await getRandomQuestionsIds();
+    const newSeries: Prisma.QuestionSeriesCreateInput = {
+      date: new Date(today),
+      difficulty: 1,
+      title: `Daily ${dailyNumber}`,
+      type: "daily",
+      userCreate: "Auto",
+      userUpdate: "Auto",
+      data: {
+        id: dailyNumber,
+        questionsIds,
+      },
+    };
+
+    return prisma.questionSeries.create({ data: newSeries });
+  }
+});
+/**
+ * Récupère l'ID minimum et maximum des questions dans la base de données.
+ */
+async function getMinMaxId() {
+  const result = await prisma.question.aggregate({
+    _max: { id: true },
+    _min: { id: true },
+  });
+
+  return { minId: result._min.id, maxId: result._max.id };
+}
+
+/**
+ * Génère un ID aléatoire compris entre minId et maxId.
+ * @param minId - L'ID minimum possible.
+ * @param maxId - L'ID maximum possible.
+ * @returns Un ID aléatoire.
+ */
+function getRandomId(minId: number, maxId: number): number {
+  return Math.floor(Math.random() * (maxId - minId + 1) + minId);
+}
+
+/**
+ * Vérifie si une question existe avec l'ID donné.
+ * @param id - L'ID de la question à vérifier.
+ * @returns True si la question existe, sinon False.
+ */
+async function isQuestionExists(id: number): Promise<boolean> {
+  const question = await prisma.question.findUnique({
+    where: { id },
+    select: { id: true },
+  });
+  return question !== null;
+}
+
+/**
+ * Récupère 10 questions aléatoires uniques existantes dans la base de données.
+ * @returns Un tableau de 10 questions uniques.
+ */
+async function getRandomQuestionsIds() {
+  const { minId, maxId } = await getMinMaxId();
+  const uniqueIds = new Set<number>();
+
+  // Continuer à générer des IDs jusqu'à ce qu'on en ait 10 uniques et existants
+  while (uniqueIds.size < 10) {
+    const randomId = getRandomId(minId!, maxId!);
+    if (await isQuestionExists(randomId)) {
+      uniqueIds.add(randomId); // Ajouter seulement si l'ID existe
+    }
+  }
+
+  return Array.from(uniqueIds);
+}
