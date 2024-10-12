83a3c33837717a6b76af77618e42cc0dff9f3e3f
L3Mars <pain.antoine@gmail.com>
2024-10-12 21:54:15 +0200

    Commit daily series part 1

---
+++ components/QuestionSeries.vue
@@ -0,0 +1,151 @@
+<template>
+  <v-skeleton-loader
+    :loading="firstLoading"
+    type="article, list-item,list-item,list-item,list-item, actions"
+    width="500"
+  >
+    <div class="d-flex flex-column justify-center" style="max-width: 500px">
+      <div class="d-flex flex-row justify-center mb-5" style="max-width: 500px">
+        <v-icon class="mr-2">mdi-help-box-multiple-outline</v-icon>
+        {{ question.themes.join(", ") }}
+        <v-spacer></v-spacer>
+        <v-progress-circular v-if="loadingReporting" indeterminate>
+        </v-progress-circular>
+        <v-icon
+          v-else
+          @click="reportQuestion()"
+          color="orange-lighten-2"
+          :disabled="reported"
+          icon="mdi-flag"
+          :loading="loadingReporting"
+        >
+        </v-icon>
+      </div>
+      <h3>{{ question.data.libelle }}</h3>
+      <v-item-group mandatory v-model="selectedResponse" class="mx-auto ma-5">
+        <v-item
+          v-for="proposition in question.data.propositions"
+          v-slot="{ isSelected, toggle }"
+        >
+          <v-btn
+            style="min-width: 250px; margin-bottom: 5px; display: block"
+            class="mx-auto"
+            :value="proposition.id"
+            :variant="isSelected ? 'tonal' : 'outlined'"
+            :color="
+              redResponse == proposition.id
+                ? 'red'
+                : greenResponse == proposition.id
+                ? 'green'
+                : isSelected
+                ? 'green'
+                : 'white'
+            "
+            @click="toggle"
+          >
+            {{ proposition.value }}
+          </v-btn>
+        </v-item>
+      </v-item-group>
+      <span class="mb-5">{{ commentaire }}</span>
+      <span
+        v-if="responded"
+        class="d-flex justify-center align-center"
+        style="position: relative"
+      >
+        <v-btn
+          @click="NextQuestion()"
+          :loading="loading"
+          class="mx-auto"
+          style="width: 250px"
+          color="blue"
+        >
+          Continuer
+        </v-btn>
+        <transition name="fade">
+          <p
+            v-if="showXP"
+            class="xp-text"
+            style="position: absolute; left: 85%"
+          >
+            + {{ xpWin }} xp
+          </p>
+        </transition>
+      </span>
+      <v-btn
+        v-if="!responded"
+        @click="validateResponse()"
+        style="width: 250px"
+        class="mx-auto"
+        color="green"
+        :disabled="selectedResponse == null"
+      >
+        Valider
+      </v-btn>
+    </div>
+  </v-skeleton-loader>
+</template>
+<script setup lang="ts">
+import { ReportingDTO } from "~/models/DTO/reportingDTO";
+import { QuestionDTO } from "~/models/question";
+
+const props = defineProps<{ question: QuestionDTO }>();
+const firstLoading = ref(true);
+const loading = ref(true);
+const loadingReporting = ref(false);
+const commentaire = ref("");
+const responded = ref(false);
+const selectedResponse = ref();
+const redResponse = ref();
+const greenResponse = ref();
+const xpWin = ref(0);
+const showXP = ref(false);
+const reported = ref(false);
+
+const emit = defineEmits(["validateResponse", "nextQuestion"]);
+async function validateResponse() {
+  emit("validateResponse");
+}
+
+async function NextQuestion() {
+  emit("nextQuestion");
+}
+
+async function reportQuestion() {
+  try {
+    loadingReporting.value = true;
+    const reportingDto = new ReportingDTO();
+    reportingDto.questionId = props.question.id;
+    await $fetch("/api/question/report", {
+      method: "post",
+      body: { ...reportingDto },
+    });
+    reported.value = true;
+  } finally {
+    loadingReporting.value = false;
+  }
+}
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.xp-text {
+  font-family: "Quicksand", sans-serif;
+  /* Ou 'Quicksand', selon ta préférence */
+  font-weight: 600;
+  font-size: 1rem;
+  /* Ajuste selon tes besoins */
+  color: #ffcc00;
+  /* Jaune or pour un effet de gain d'XP */
+  text-align: center;
+}
+</style>
--- pages/series/daily.vue
+++ pages/series/daily.vue
@@ -1,9 +1,12 @@
 <template>
   <v-card flat rounded class="mx-auto my-auto pa-5">
-    <Question :theme="route.params.id"></Question>
+    <QuestionSeries :question="route.params.id"></QuestionSeries>
   </v-card>
 </template>
 <script setup>
+import { QuestionDTO } from "~/models/question";
+
 const route = useRoute();
+const question = ref(new QuestionDTO());
 const { data: series } = await useFetch("/api/series/daily");
 </script>
