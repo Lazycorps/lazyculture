9b3ef4c8895394d8a38970d17c2b2a2210889871
Antoine Pain <apain@biac.lu>
2024-10-12 23:01:03 +0200

    deailyseries part 2

--- pages/series/daily.vue
+++ pages/series/daily.vue
@@ -1,12 +1,33 @@
 <template>
   <v-card flat rounded class="mx-auto my-auto pa-5">
-    <QuestionSeries :question="route.params.id"></QuestionSeries>
+    {{ series }}
+    <v-btn @click="startSeries">Start</v-btn>
+    <QuestionSeries v-if="question" :question="question"></QuestionSeries>
   </v-card>
 </template>
-<script setup>
+<script setup lang="ts">
 import { QuestionDTO } from "~/models/question";
+import type { QuestionSeriesDTO } from "~/models/series";
 
-const route = useRoute();
-const question = ref(new QuestionDTO());
-const { data: series } = await useFetch("/api/series/daily");
+const question = ref<QuestionDTO | null>(null);
+const loading = ref(false);
+const { data: series } = await useFetch<QuestionSeriesDTO>("/api/series/daily");
+
+async function startSeries() {
+  try {
+    loading.value = true;
+    question.value = await NextQuestion();
+  } finally {
+    loading.value = false;
+  }
+}
+
+async function NextQuestion() {
+  try {
+    loading.value = true;
+    return await $fetch<QuestionDTO>("/api/question/" + series.value?.data.questionsIds[0]);
+  } finally {
+    loading.value = false;
+  }
+}
 </script>
