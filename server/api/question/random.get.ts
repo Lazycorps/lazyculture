c4c70fa775a0b1a6edee13156a82f87c4a6b9181
ImCraZiggs <ludovic.dabee@gmail.com>
2024-10-12 20:09:42 +0200

    Shuffle Answers

--- server/api/question/random.get.ts
+++ server/api/question/random.get.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { serverSupabaseClient } from "#supabase/server";
 import { QuestionDataDTO } from "~/models/question";
 import prisma from "~/lib/prisma";
+import { shuffle } from 'lodash';
 
 export default defineEventHandler(async (event) => {
   const client = await serverSupabaseClient(event);
@@ -15,7 +16,10 @@ export default defineEventHandler(async (event) => {
   const id = getRandomId(ids);
   const question = await prisma.question.findFirst({ where: { id: id } });
   if (question) {
-    const questionThemes = (question.data as any as QuestionDataDTO).theme;
+    const questionData = question.data as any as QuestionDataDTO;
+    questionData.propositions = shuffle(questionData.propositions);
+
+    const questionThemes = questionData.theme;
     const themes = await prisma.questionTheme.findMany({
       where: { slug: { in: questionThemes } },
     });
