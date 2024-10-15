65cc65ac16d2f3617baa2d3bdd36ba5025d88512
L3Mars <pain.antoine@gmail.com>
2024-10-15 22:57:05 +0200

    Add random button for theme

--- components/Theme.vue
+++ components/Theme.vue
@@ -1,13 +1,26 @@
 <template>
-  <v-card rounded outlined @click="router.push('/themes/' + theme.slug)" class="ma-1">
+  <v-card
+    rounded
+    outlined
+    @click="router.push('/themes/' + theme.slug)"
+    class="ma-1"
+  >
     <v-img :src="theme.picture" height="200px"></v-img>
     <v-card-title>{{ theme.name }}</v-card-title>
     <v-card-actions>
-      <v-progress-linear :indeterminate="loading" :model-value="responseCount" :max="questionCount" min="0"
-        color="primary" height="10" rounded=""></v-progress-linear>
+      <v-progress-linear
+        :indeterminate="loading"
+        :model-value="responseCount"
+        :max="questionCount"
+        min="0"
+        color="primary"
+        height="10"
+        rounded=""
+      ></v-progress-linear>
       <div class="text-caption">{{ responseCount }}/{{ questionCount }}</div>
-      <div v-if="mastery > 0" class="text-caption d-flex"><v-icon class="mr-1">mdi-arm-flex</v-icon> {{
-        mastery.toFixed(1) }}</div>
+      <div v-if="mastery > 0" class="text-caption d-flex">
+        <v-icon class="mr-1">mdi-arm-flex</v-icon> {{ mastery.toFixed(1) }}
+      </div>
     </v-card-actions>
   </v-card>
 </template>
@@ -24,6 +37,7 @@ const loading = ref(false);
 onMounted(async () => {
   await loadProgress();
 });
+
 async function loadProgress() {
   try {
     loading.value = true;
@@ -36,6 +50,5 @@ async function loadProgress() {
   } finally {
     loading.value = false;
   }
-
 }
 </script>
