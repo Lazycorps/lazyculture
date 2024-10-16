001ad5e9b00aa6ac0a86ec542dbdf28e3d3cf0d7
Gloumy <simon@cornet.dev>
2024-10-16 23:22:25 +0200

    affichage (basique) des différentes questions

--- pages/admin/importquestions.vue
+++ pages/admin/importquestions.vue
@@ -1,8 +1,24 @@
 <template>
   <v-card flat rounded class="d-flex flex-column pa-5">
-    <h2>Import des questions</h2>
+    <h1>Import des questions</h1>
     <v-textarea label="JSON source" rows="5" v-model="sourceJson" />
-    <v-btn @click="loadJsonIntoQuestions">Charger</v-btn>
+    <span
+      ><v-btn @click="loadJsonIntoQuestions">Charger</v-btn
+      ><!-- <v-btn>Ajouter</v-btn> --></span
+    >
+    <h2>Questions</h2>
+    <div v-for="(question, index) in questionsfromJson" style="overflow-y: scroll;">
+      <h3>{{ index + 1 }}.</h3>
+      <v-text-field label="Intitulé" v-model="question.libelle" />
+      <span class="d-flex">
+        <v-select label="Type" v-model="question.type" />
+        <v-select label="Difficulté" v-model="question.difficulty" />
+        <v-combobox multiple chips label="Thème(s)" v-model="question.theme" />
+      </span>
+      <span v-for="(prop, propIndex) in question.propositions" class="d-flex">        
+        <v-text-field :label="`Réponse ${propIndex + 1}`" :model-value="prop.value" :class="question.response == propIndex + 1 ? 'text-green' : ''" />
+      </span>
+    </div>
   </v-card>
 </template>
 
@@ -15,7 +31,7 @@ const questionsfromJson = ref<QuestionDataDTO[]>([]);
 
 function loadJsonIntoQuestions() {
   const json: QuestionDataDTO[] = JSON.parse(sourceJson.value);
-  
+
   questionsfromJson.value = [];
 
   for (var key in Object.keys(json)) {
