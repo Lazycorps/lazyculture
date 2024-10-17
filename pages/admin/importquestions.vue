d8d9d5a66973ea83b494d6bfb84dab107cede520
Gloumy <simon@cornet.dev>
2024-10-17 14:19:27 +0200

    wip- bouton d'import - 401 unauthorized

--- pages/admin/importquestions.vue
+++ pages/admin/importquestions.vue
@@ -6,19 +6,27 @@
       ><v-btn @click="loadJsonIntoQuestions">Charger</v-btn
       ><!-- <v-btn>Ajouter</v-btn> --></span
     >
-    <div v-if="questionsfromJson.length > 0" style="overflow-y: scroll" class="d-flex flex-shrink-1 flex-column">
+    <div
+      v-if="questionsfromJson.length > 0"
+      style="overflow-y: scroll"
+      class="d-flex flex-shrink-1 flex-column"
+    >
       <h2>{{ questionsfromJson.length }} questions</h2>
       <v-checkbox v-model="hideAnswers" label="Cacher la bonne réponse" />
-      <div
-        v-for="(question, index) in questionsfromJson"        
-      >
+      <div v-for="(question, index) in questionsfromJson">
         <span class="d-flex justify-space-between"
           ><h3>{{ index + 1 }}.</h3>
-          <v-btn density="compact" @click="removeFromQuestions(index)" color="red" text="Supprimer"
+          <v-btn
+            density="compact"
+            @click="removeFromQuestions(index)"
+            color="red"
+            text="Supprimer"
         /></span>
         <QuestionForm :question="question" :hide-answers="hideAnswers" />
       </div>
+      <v-btn color="blue" text="Importer les questions" @click="sendToApi" />
     </div>
+
   </v-card>
 </template>
 
@@ -27,6 +35,7 @@ import { QuestionDataDTO } from "~/models/question";
 import QuestionForm from "~/components/admin/importquestions/QuestionForm.vue";
 
 const hideAnswers = ref(true);
+const loading = ref(false);
 const sourceJson = ref("");
 
 const questionsfromJson = ref<QuestionDataDTO[]>([]);
@@ -44,5 +53,20 @@ function loadJsonIntoQuestions() {
 
 function removeFromQuestions(index: number) {
   questionsfromJson.value.splice(index, 1);
+}
+
+async function sendToApi() {
+  try {
+    loading.value = true;
+    if (questionsfromJson.value.length == 0) return;
+    await $fetch("/api/import/chatgpt", {
+      method: "post",
+      body: questionsfromJson.value      
+    });
+  } finally {
+    // sourceJson.value = "";
+    // questionsfromJson.value = [];
+    loading.value = false;
+  }
 }
 </script>
