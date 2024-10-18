497456d7cd785abad8540acf8ee179f21594ae44
ImCraZiggs <ludovic.dabee@gmail.com>
2024-10-18 22:51:09 +0200

    Create Admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,73 @@
+<template>
+    <v-layout>
+        <v-navigation-drawer app width="200" permanent>
+            <v-list>
+                <v-list-item-group>
+                    <v-list-item>
+                        <v-list-item-content>
+                            <v-list-item-title class="headline">Maintenance</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+
+                    <v-list-item horizontal @click="router.push('admin/maintenance/questions')">
+                        <template v-slot:prepend>
+                            <v-icon>mdi-help-circle</v-icon>
+                        </template>
+
+                        <v-list-item-content>
+                            <v-list-item-title>Questions</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+
+                    <v-list-item @click="router.push('/themes')">
+                        <template v-slot:prepend>
+                            <v-icon>mdi-book-open-variant-outline</v-icon>
+                        </template>
+                        <v-list-item-content>
+                            <v-list-item-title>Themes</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+
+                    <v-list-item @click="router.push('/admin/importquestions')">
+                        <template v-slot:prepend>
+                            <v-icon>mdi-import</v-icon>
+                        </template>
+                        <v-list-item-content>
+                            <v-list-item-title>Imports</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-main class="page-content">
+            <slot />
+        </v-main>
+    </v-layout>
+</template>
+
+<script setup lang="ts">
+const router = useRouter();
+</script>
+
+<style>
+html,
+body,
+#app,
+.v-application {
+    height: 100%;
+    margin: 0;
+}
+
+.page-content {
+    height: 100vh;
+    /* 100% de la hauteur de la page */
+    overflow-y: auto;
+    /* Ajoute une scrollbar uniquement si nécessaire pour le contenu */
+}
+
+.headline {
+    padding: 16px;
+    /* Optionnel : ajustez le remplissage pour le titre */
+}
+</style>
--- pages/admin/maintenance/questions.vue
+++ pages/admin/maintenance/questions.vue
@@ -1,99 +1,106 @@
 <template>
-  <v-card flat rounded class="my-auto mx-auto pa-5" style="max-width: 1500px; min-width: 800px;">
-    <v-data-table :headers="computedHeaders" :items="filteredQuestions" :items-per-page="10" fixed-header height="100%"
-      style="max-height: 650px;" class="fill-width">
-      <template v-slot:top>
-        <v-toolbar class="pt-4 pl-4">
-          <v-text-field label="Filtre textuel" underlined density="compact" class="mr-10" width="200px"
-            v-model="textFilter" />
-          <v-combobox label="Thèmes" :items="themes?.map(theme => theme.name) ?? []" v-model="selectedTheme"
-            density="compact" width="200px" class="mr-10" clearable></v-combobox>
-          <v-switch v-model="showReportedQuestions" color="green" label="Questions Signalées" class="mr-10"></v-switch>
-          <v-switch color="green" label="Afficher réponses" v-model="showResponse"></v-switch>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="editItem(defaultItem)" :disabled="false" class="rounded-circle mr-6 mb-4"
-            style="background-color: green; color: white;">
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.response="{ item }">
-        <span>{{ getResponse(item.data) }}</span>
-      </template>
-      <template v-slot:item.themeNames="{ item }">
-        <span>{{ getThemNames(item.data.theme) }}</span>
-      </template>
-      <template v-slot:item.reported="{ item }">
-        <div class="d-flex align-center justify-center">
-          <v-icon v-if="isReported(item.reportings)" color="orange-lighten-2" icon="mdi-flag" />
-        </div>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-icon class="me-2" size="small" @click="editItem(item)">
-          mdi-pencil
-        </v-icon>
-        <v-icon size="small" @click="deleteItem(item)" :disabled="true">
-          mdi-delete
-        </v-icon>
-      </template>
-    </v-data-table>
-    <v-dialog v-model="dialog" max-width="800px">
-      <v-card>
-        <v-card-title>{{ formTitle }}</v-card-title>
-        <v-card-text>
-          <v-textarea v-model="editedItem.data.libelle" label="Libellé de la question" rows="2" auto-grow
-            density="compact" />
-          <v-row>
-            <v-col cols="6">
-              <v-radio-group v-model="editedItem.data.response">
-                <div v-for="(proposition, index) in paddedPropositions" :key="index" class="d-flex align-center">
-                  <v-text-field v-model="proposition.value" :label="'Proposition ' + (index + 1)"
-                    class="flex-grow-1 mr-4" density="compact" width="260"  @blur="updateProposition(index, proposition.value)" />
-                  <v-radio :value="proposition.id"
-                    :disabled="!proposition.value.trim()"
-                    :style="{ color: editedItem.data.response === proposition.id ? 'green' : 'red' }" />
-                </div>
-              </v-radio-group>
-            </v-col>
-            <v-col cols="6">
-              <v-combobox v-model="selectedThemeNames" :items="themes?.map(theme => theme.name) ?? []" label="Thèmes"
-                multiple hide-selected chips closable-chips density="compact" />
-              <v-text-field v-model="editedItem.data.img" label="URL de l'image" density="compact" />
-            </v-col>
-          </v-row>
-          <v-textarea v-model="editedItem.data.commentaire" label="Commentaire" rows="2" auto-grow density="compact" />
-          <v-divider class="my-1"></v-divider>
-          <div v-if="editedItem.reportings.length">
-            <h3>Signalements</h3>
-            <div v-for="(reporting, index) in editedItem.reportings" :key="index" class="d-flex align-center mt-2">
-              <v-text-field v-model="reporting.commentaire" :label="'Commentaire ' + (index + 1)" readonly
-                class="flex-grow-1 mr-4" />
-              <v-checkbox v-model="reporting.closed" label="Clôturé" />
-            </div>
+  <NuxtLayout name="admin">
+    <v-card flat rounded class="my-auto mx-auto pa-5" style="max-width: 1500px; min-width: 800px;">
+      <v-data-table :headers="computedHeaders" :items="filteredQuestions" :items-per-page="10" fixed-header
+        height="100%" style="max-height: 650px;" class="fill-width">
+        <template v-slot:top>
+          <v-toolbar class="pt-4 pl-4">
+            <v-text-field label="Filtre textuel" underlined density="compact" class="mr-10" width="200px"
+              v-model="textFilter" />
+            <v-combobox label="Thèmes" :items="themes?.map(theme => theme.name) ?? []" v-model="selectedTheme"
+              density="compact" width="200px" class="mr-10" clearable></v-combobox>
+            <v-switch v-model="showReportedQuestions" color="green" label="Questions Signalées"
+              class="mr-10"></v-switch>
+            <v-switch color="green" label="Afficher réponses" v-model="showResponse"></v-switch>
+            <v-spacer></v-spacer>
+            <v-btn icon @click="editItem(defaultItem)" :disabled="false" class="rounded-circle mr-6 mb-4"
+              style="background-color: green; color: white;">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.response="{ item }">
+          <span>{{ getResponse(item.data) }}</span>
+        </template>
+        <template v-slot:item.themeNames="{ item }">
+          <span>{{ getThemNames(item.data.theme) }}</span>
+        </template>
+        <template v-slot:item.reported="{ item }">
+          <div class="d-flex align-center justify-center">
+            <v-icon v-if="isReported(item.reportings)" color="orange-lighten-2" icon="mdi-flag" />
           </div>
-        </v-card-text>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon class="me-2" size="small" @click="editItem(item)">
+            mdi-pencil
+          </v-icon>
+          <v-icon size="small" @click="deleteItem(item)" :disabled="true">
+            mdi-delete
+          </v-icon>
+        </template>
+      </v-data-table>
+      <v-dialog v-model="dialog" max-width="800px">
+        <v-card>
+          <v-card-title>{{ formTitle }}</v-card-title>
+          <v-card-text>
+            <v-textarea v-model="editedItem.data.libelle" label="Libellé de la question" rows="2" auto-grow
+              density="compact" />
+            <v-row>
+              <v-col cols="6">
+                <v-radio-group v-model="editedItem.data.response">
+                  <div v-for="(proposition, index) in paddedPropositions" :key="index" class="d-flex align-center">
+                    <v-text-field v-model="proposition.value" :label="'Proposition ' + (index + 1)"
+                      class="flex-grow-1 mr-4" density="compact" width="260"
+                      @blur="updateProposition(index, proposition.value)" />
+                    <v-radio :value="proposition.id" :disabled="!proposition.value.trim()"
+                      :style="{ color: editedItem.data.response === proposition.id ? 'green' : 'red' }" />
+                  </div>
+                </v-radio-group>
+              </v-col>
+              <v-col cols="6">
+                <v-combobox v-model="selectedThemeNames" :items="themes?.map(theme => theme.name) ?? []" label="Thèmes"
+                  multiple hide-selected chips closable-chips density="compact" />
+                <v-text-field v-model="editedItem.data.img" label="URL de l'image" density="compact" />
+              </v-col>
+            </v-row>
+            <v-textarea v-model="editedItem.data.commentaire" label="Commentaire" rows="2" auto-grow
+              density="compact" />
+            <v-divider class="my-1"></v-divider>
+            <div v-if="editedItem.reportings.length">
+              <h3>Signalements</h3>
+              <div v-for="(reporting, index) in editedItem.reportings" :key="index" class="d-flex align-center mt-2">
+                <v-text-field v-model="reporting.commentaire" :label="'Commentaire ' + (index + 1)" readonly
+                  class="flex-grow-1 mr-4" />
+                <v-checkbox v-model="reporting.closed" label="Clôturé" />
+              </div>
+            </div>
+          </v-card-text>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn @click="close">Annuler</v-btn>
-          <v-btn @click="save" :disabled="isSaveDisabled">Sauvegarder</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-dialog v-model="dialogDelete" max-width="500px">
-      <v-card>
-        <v-card-title>Voulez-vous supprimer cette question ?</v-card-title>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn @click="closeDelete" color="orange-lighten-3">Annuler</v-btn>
-          <v-btn color="error" @click="deleteItemConfirm">Supprimer</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-card>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="close">Annuler</v-btn>
+            <v-btn @click="save" :disabled="isSaveDisabled">Sauvegarder</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="dialogDelete" max-width="500px">
+        <v-card>
+          <v-card-title>Voulez-vous supprimer cette question ?</v-card-title>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="closeDelete" color="orange-lighten-3">Annuler</v-btn>
+            <v-btn color="error" @click="deleteItemConfirm">Supprimer</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-card>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
 import { ref, computed } from 'vue';
 import type { VDataTable } from 'vuetify/components'
 import type { QuestionDataDTO, QuestionDTO, QuestionPropositionDTO, QuestionReportingDTO } from '~/models/question';
@@ -290,7 +297,7 @@ function closeDelete() {
 };
 
 const isSaveDisabled = computed(() => {
-  const libelleValid = editedItem.value.data.libelle.trim() !== ""; 
+  const libelleValid = editedItem.value.data.libelle.trim() !== "";
   const propositionsValid = editedItem.value.data.propositions.filter(p => p.value.trim() !== "").length >= 2;
   const radioSelected = editedItem.value.data.response !== 0;
 
@@ -325,7 +332,7 @@ async function save() {
         ...editedItem.value.data,
       }
     };
-      await $fetch("/api/question/create", {
+    await $fetch("/api/question/create", {
       method: "post",
       body: { ...newQuestion },
     });
