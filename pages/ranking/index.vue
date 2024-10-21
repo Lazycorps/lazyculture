f7c71d25d3818a3997a68b4b9069b61597530553
L3Mars <pain.antoine@gmail.com>
2024-10-21 23:02:35 +0200

    Add user ascend in ranking

--- pages/ranking/index.vue
+++ pages/ranking/index.vue
@@ -1,30 +1,41 @@
 <template>
-    <v-card flat rounded class="my-auto mx-auto pa-5" style="max-width: 600px; min-width: 400px;">
-        <v-list>
-            <v-list-item v-for="(user, key) in users" :key="user.userId" class="mx-auto">
-                <template v-slot:prepend>
-                    <v-avatar :color="getColorByRank(key + 1)">
-                        {{ key + 1 }}
-                    </v-avatar>
-                </template>
-                {{ user.user.name ? user.user.name : "Anonymous" }}
-                <template v-slot:append>
-                    {{ user.xp }}
-                </template>
-            </v-list-item>
-        </v-list>
-    </v-card>
+  <v-card
+    flat
+    rounded
+    class="my-auto mx-auto pa-5"
+    style="max-width: 600px; min-width: 400px"
+  >
+    <v-list>
+      <v-list-item
+        v-for="(user, key) in users"
+        :key="user.userId"
+        class="mx-auto"
+      >
+        <template v-slot:prepend>
+          <v-avatar :color="getColorByRank(key + 1)">
+            {{ key + 1 }}
+          </v-avatar>
+        </template>
+        {{ user.name ? user.name : "Anonymous" }}
+        <template v-slot:append>
+          <div class="mr-5" style="width: 100px; text-align: end">
+            {{ user.xp }}
+          </div>
+          <div style="width: 50px; text-align: end">{{ user.bestAscend }}</div>
+        </template>
+      </v-list-item>
+    </v-list>
+  </v-card>
 </template>
 
 <script setup lang="ts">
-
 const { data: users } = await useFetch("/api/ranking/top");
 
 function getColorByRank(rank: number) {
-    if (rank == 1) return "#FFD700";
-    if (rank == 2) return "#C0C0C0";
-    if (rank == 3) return "#CD7F32";
-    return "";
+  if (rank == 1) return "#FFD700";
+  if (rank == 2) return "#C0C0C0";
+  if (rank == 3) return "#CD7F32";
+  return "";
 }
 </script>
 
--- server/api/ranking/top.get.ts
+++ server/api/ranking/top.get.ts
@@ -1,11 +1,43 @@
 import prisma from "~/lib/prisma";
+import { UserRankingDTO } from "~/models/DTO/userRankingDTO";
 
 export default defineEventHandler(async (event) => {
-  return await prisma.userProgress.findMany({
+  const userProgress = await prisma.userProgress.findMany({
     include: {
       user: true,
     },
     orderBy: [{ xp: "desc" }],
     take: 20,
   });
+
+  const topUsersAscend = await prisma.questionSeriesResponse.groupBy({
+    by: ["userId"], // Grouper par utilisateur
+    where: {
+      userId: { in: userProgress.map((up) => up.userId) },
+      seriesType: "ascent",
+    },
+    _max: {
+      result: true, // Prendre le meilleur résultat de chaque utilisateur
+    },
+    orderBy: {
+      _max: {
+        result: "desc", // Trier par le meilleur résultat décroissant
+      },
+    },
+    take: 10, // Par exemple, pour récupérer les 10 meilleurs utilisateurs
+  });
+
+  const usersRanking: UserRankingDTO[] = [];
+  userProgress.forEach((u) => {
+    usersRanking.push({
+      name: u.user.name,
+      userId: u.userId,
+      xp: u.xp,
+      bestAscend:
+        topUsersAscend
+          ?.filter((user) => user.userId == u.userId)[0]
+          ?._max.result?.toNumber() ?? 0,
+    });
+  });
+  return usersRanking;
 });
